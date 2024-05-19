f4dfd611fa0e96e40ab7c558098cd86985780c8b
KodaiYamashita <nglcobdai@gmail.com>
2024-05-19 20:46:11 +0900

    debug: whisper test

--- tests/test_authenticator.py
+++ tests/test_authenticator.py
@@ -1,14 +1,25 @@
-# from unittest import TestCase
+# tests/test_import_whisper.py
+# import whisper
 
-# from app.api.src.authenticator import SecretPhraseAuthenticator
+# from app.api.src.transcription import Transcription
 
 
-# class TestSecretPhraseAuthenticator(TestCase):
-#     def setup_class(cls):
-#         cls.authenticator = SecretPhraseAuthenticator()
-#         cls.test_file_path = "sample/test.wav"
+from whisper import load_model
 
-#     def test_call(self):
-#         with open(self.test_file_path, "rb") as f:
-#             content = f.read()
-#         assert self.authenticator(content, "ひらけごま")
+from app.utils.config import settings
+
+
+class Transcription:
+    def __init__(self):
+        self.model = load_model(settings.WHISPER_MODEL)
+
+    def __call__(self, content):
+        return self.model.transcribe(content)
+
+
+def test_import_whisper():
+    tran = Transcription()
+    # model = whisper.load_model("small")
+    test_file_path = "sample/test.wav"
+
+    assert tran(test_file_path)["text"] == "ひらけごま"
